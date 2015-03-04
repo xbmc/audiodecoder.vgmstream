09ab398131e65698749e47dc64a2e90ee33ff1a5
Sascha Montellese <sascha.montellese@scs.ch>
2015-03-04 11:34:22 +0100

    [win32] use stdint.h instead of custom pstdint.h

--- lib/vgmstream/src/streamtypes.h
+++ lib/vgmstream/src/streamtypes.h
@@ -6,13 +6,12 @@
 #ifndef _STREAMTYPES_H
 #define _STREAMTYPES_H
 
+#include <stdint.h>
 #ifdef _MSC_VER
-#include <pstdint.h>
+//#include <pstdint.h>
 #define inline _inline
 #define strcasecmp _stricmp
 #define snprintf _snprintf
-#else
-#include <stdint.h>
 #endif
 
 typedef int16_t sample;
