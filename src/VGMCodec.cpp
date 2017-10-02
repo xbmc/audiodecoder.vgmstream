8bd6ca18d07dabc99c2e8d4636f860c2d8b28697
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2017-10-02 13:25:39 +0200

    update to c++ bindings

--- src/VGMCodec.cpp
+++ src/VGMCodec.cpp
@@ -18,244 +18,191 @@
  *
  */
 
-#include "libXBMC_addon.h"
-
-ADDON::CHelper_libXBMC_addon *XBMC           = NULL;
+#include <kodi/addon-instance/AudioDecoder.h>
+#include <kodi/Filesystem.h>
 
 extern "C" {
-
 #include "src/vgmstream.h"
-#include "kodi_audiodec_dll.h"
-#include "IFileTypes.h"
-#include "AEChannelData.h"
-
-//-- Create -------------------------------------------------------------------
-// Called on load. Addon should fully initalize or return error status
-//-----------------------------------------------------------------------------
-ADDON_STATUS ADDON_Create(void* hdl, void* props)
-{
-  if (!XBMC)
-    XBMC = new ADDON::CHelper_libXBMC_addon;
-
-  if (!XBMC->RegisterMe(hdl))
-  {
-    delete XBMC, XBMC=NULL;
-    return ADDON_STATUS_PERMANENT_FAILURE;
-  }
-
-  return ADDON_STATUS_OK;
-}
-
-//-- Destroy ------------------------------------------------------------------
-// Do everything before unload of this add-on
-// !!! Add-on master function !!!
-//-----------------------------------------------------------------------------
-void ADDON_Destroy()
-{
-  XBMC=NULL;
-}
-
-//-- GetStatus ---------------------------------------------------------------
-// Returns the current Status of this visualisation
-// !!! Add-on master function !!!
-//-----------------------------------------------------------------------------
-ADDON_STATUS ADDON_GetStatus()
-{
-  return ADDON_STATUS_OK;
-}
-
-//-- SetSetting ---------------------------------------------------------------
-// Set a specific Setting value (called from XBMC)
-// !!! Add-on master function !!!
-//-----------------------------------------------------------------------------
-ADDON_STATUS ADDON_SetSetting(const char *strSetting, const void* value)
-{
-  return ADDON_STATUS_OK;
-}
 
 struct VGMContext
 {
   STREAMFILE sf;
-  void* file;
+  kodi::vfs::CFile* file = nullptr;
   char name[260];
-  VGMSTREAM* stream;
+  VGMSTREAM* stream = nullptr;
   size_t pos;
 };
 
-static size_t read_XBMC(struct _STREAMFILE* streamfile, uint8_t* dest, off_t offset, size_t length)
+
+static size_t read_VFS(struct _STREAMFILE* streamfile, uint8_t* dest, off_t offset, size_t length)
 {
   VGMContext *ctx = (VGMContext*) streamfile;
-  if (ctx && ctx->file && XBMC)
+  if (ctx && ctx->file)
   {
-    XBMC->SeekFile(ctx->file, offset, SEEK_SET);
-    return XBMC->ReadFile(ctx->file, dest, length);
+    ctx->file->Seek(offset, SEEK_SET);
+    return ctx->file->Read(dest, length);
   }
   return 0;
 }
 
-static void close_XBMC(struct _STREAMFILE* streamfile)
+static void close_VFS(struct _STREAMFILE* streamfile)
 {
   VGMContext *ctx = (VGMContext*) streamfile;
-  if (ctx && ctx->file && XBMC)
-    XBMC->CloseFile(ctx->file);
+  if (ctx && ctx->file)
+    delete ctx->file;
   ctx->file = nullptr;
 }
 
-static size_t get_size_XBMC(struct _STREAMFILE* streamfile)
+static size_t get_size_VFS(struct _STREAMFILE* streamfile)
 {
   VGMContext *ctx = (VGMContext*) streamfile;
-  if (ctx && ctx->file && XBMC)
-    return XBMC->GetFileLength(ctx->file);
+  if (ctx && ctx->file)
+    return ctx->file->GetLength();
 
   return 0;
 }
 
-static off_t get_offset_XBMC(struct _STREAMFILE* streamfile)
+static off_t get_offset_VFS(struct _STREAMFILE* streamfile)
 {
   VGMContext *ctx = (VGMContext*) streamfile;
-  if (ctx && ctx->file && XBMC)
-    return XBMC->GetFilePosition(ctx->file);
+  if (ctx && ctx->file)
+    return ctx->file->GetPosition();
 
   return 0;
 }
 
-static void get_name_XBMC(struct _STREAMFILE* streamfile, char* buffer, size_t length)
+static void get_name_VFS(struct _STREAMFILE* streamfile, char* buffer, size_t length)
 {
   VGMContext *ctx = (VGMContext*) streamfile;
   if (ctx)
     strcpy(buffer, ctx->name);
 }
 
-static struct _STREAMFILE* open_XBMC(struct _STREAMFILE* streamfile, const char* const filename, size_t buffersize)
+static struct _STREAMFILE* open_VFS(struct _STREAMFILE* streamfile, const char* const filename, size_t buffersize)
 {
   VGMContext *ctx = (VGMContext*) streamfile;
   ctx->pos = 0;
-  ctx->file = XBMC->OpenFile(filename, XFILE::READ_CACHED);
-  ctx->sf.read = read_XBMC;
-  ctx->sf.get_size = get_size_XBMC;
-  ctx->sf.get_offset = get_offset_XBMC;
-  ctx->sf.get_name = get_name_XBMC;
-  ctx->sf.get_realname = get_name_XBMC;
-  ctx->sf.open = open_XBMC;
-  ctx->sf.close = close_XBMC;
+  ctx->file = new kodi::vfs::CFile;
+  ctx->file->OpenFile(filename, READ_CACHED);
+  ctx->sf.read = read_VFS;
+  ctx->sf.get_size = get_size_VFS;
+  ctx->sf.get_offset = get_offset_VFS;
+  ctx->sf.get_name = get_name_VFS;
+  ctx->sf.get_realname = get_name_VFS;
+  ctx->sf.open = open_VFS;
+  ctx->sf.close = close_VFS;
   strcpy(ctx->name, filename);
 
   return streamfile;
 }
 
-#define SET_IF(ptr, value) \
-{ \
-  if ((ptr)) \
-   *(ptr) = (value); \
 }
 
-void* Init(const char* strFile, unsigned int filecache, int* channels,
-           int* samplerate, int* bitspersample, int64_t* totaltime,
-           int* bitrate, AEDataFormat* format, const AEChannel** channelinfo)
+class CVGMCodec : public kodi::addon::CInstanceAudioDecoder,
+                  public kodi::addon::CAddonBase
 {
-  if (!strFile)
-    return NULL;
+public:
+  CVGMCodec(KODI_HANDLE instance) :
+    CInstanceAudioDecoder(instance) {}
 
-  VGMContext* result = new VGMContext;
-  if (!result)
-    return NULL;
-
-  open_XBMC((struct _STREAMFILE*)result, strFile, 0);
-
-  result->stream = init_vgmstream_from_STREAMFILE((struct _STREAMFILE*)result);
-  if (!result->stream)
+  virtual ~CVGMCodec()
   {
-    delete result;
-    return NULL;
+    if (ctx.stream)
+      close_vgmstream(ctx.stream);
+
+    delete ctx.file;
   }
 
-  SET_IF(channels, result->stream->channels)
-  SET_IF(samplerate, result->stream->sample_rate)
-  SET_IF(bitspersample, 16)
-  SET_IF(totaltime, result->stream->num_samples/result->stream->sample_rate*1000)
-  SET_IF(format, AE_FMT_S16NE)
-  static enum AEChannel map[8][9] = {
-    {AE_CH_FC, AE_CH_NULL},
-    {AE_CH_FL, AE_CH_FR, AE_CH_NULL},
-    {AE_CH_FL, AE_CH_FC, AE_CH_FR, AE_CH_NULL},
-    {AE_CH_FL, AE_CH_FR, AE_CH_BL, AE_CH_BR, AE_CH_NULL},
-    {AE_CH_FL, AE_CH_FC, AE_CH_FR, AE_CH_BL, AE_CH_BR, AE_CH_NULL},
-    {AE_CH_FL, AE_CH_FC, AE_CH_FR, AE_CH_BL, AE_CH_BR, AE_CH_LFE, AE_CH_NULL},
-    {AE_CH_FL, AE_CH_FC, AE_CH_FR, AE_CH_SL, AE_CH_SR, AE_CH_BL, AE_CH_BR, AE_CH_NULL},
-    {AE_CH_FL, AE_CH_FC, AE_CH_FR, AE_CH_SL, AE_CH_SR, AE_CH_BL, AE_CH_BR, AE_CH_LFE, AE_CH_NULL}
-  };
-
-  SET_IF(channelinfo, NULL)
-  if (result->stream->channels <= 8)
-    SET_IF(channelinfo, map[result->stream->channels-1])
-
-  SET_IF(bitrate, 0)
-
-  return result;
-}
-
-int ReadPCM(void* context, uint8_t* pBuffer, int size, int *actualsize)
-{
-  if (!context || !actualsize)
-    return 1;
-
-  VGMContext* ctx = (VGMContext*)context;
-
-  render_vgmstream((sample*)pBuffer, size/(2*ctx->stream->channels), ctx->stream);
-  *actualsize = size;
-
-  ctx->pos += size;
-  return 0;
-}
-
-int64_t Seek(void* context, int64_t time)
-{
-  if (!context)
-    return 0;
-
-  VGMContext* ctx = (VGMContext*)context;
-  int16_t* buffer = new int16_t[576*ctx->stream->channels];
-  if (!buffer)
-    return 0;
-
-  long samples_to_do = (long)time * ctx->stream->sample_rate / 1000L;
-  if (samples_to_do < ctx->stream->current_sample )
-     reset_vgmstream(ctx->stream);
-  else
-    samples_to_do -= ctx->stream->current_sample;
-      
-  while (samples_to_do > 0)
+  virtual bool Init(const std::string& filename, unsigned int filecache,
+                    int& channels, int& samplerate,
+                    int& bitspersample, int64_t& totaltime,
+                    int& bitrate, AEDataFormat& format,
+                    std::vector<AEChannel>& channellist) override
   {
-    long l = samples_to_do>576?576:samples_to_do;
-    render_vgmstream(buffer, l, ctx->stream);
-    samples_to_do -= l;
-  }
-  delete[] buffer;
+    open_VFS((struct _STREAMFILE*)&ctx, filename.c_str(), 0);
 
-  return time;
-}
+    ctx.stream = init_vgmstream_from_STREAMFILE((struct _STREAMFILE*)&ctx);
+    if (!ctx.stream)
+    {
+      close_VFS((struct _STREAMFILE*)&ctx);
+      return false;
+    }
 
-bool DeInit(void* context)
-{
-  if (!context)
+    channels = ctx.stream->channels;
+    samplerate = ctx.stream->sample_rate;
+    bitspersample =  16;
+    totaltime =  ctx.stream->num_samples/ctx.stream->sample_rate*1000;
+    format = AE_FMT_S16NE;
+
+    static std::vector<std::vector<enum AEChannel>> map = {
+      {AE_CH_FC},
+      {AE_CH_FL, AE_CH_FR},
+      {AE_CH_FL, AE_CH_FC, AE_CH_FR},
+      {AE_CH_FL, AE_CH_FR, AE_CH_BL, AE_CH_BR},
+      {AE_CH_FL, AE_CH_FC, AE_CH_FR, AE_CH_BL, AE_CH_BR},
+      {AE_CH_FL, AE_CH_FC, AE_CH_FR, AE_CH_BL, AE_CH_BR, AE_CH_LFE},
+      {AE_CH_FL, AE_CH_FC, AE_CH_FR, AE_CH_SL, AE_CH_SR, AE_CH_BL, AE_CH_BR},
+      {AE_CH_FL, AE_CH_FC, AE_CH_FR, AE_CH_SL, AE_CH_SR, AE_CH_BL, AE_CH_BR, AE_CH_LFE}
+      };
+
+    if (ctx.stream->channels <= 8)
+      channellist = map[ctx.stream->channels-1];
+
+    bitrate = 0;
+
     return true;
+  }
 
-  VGMContext* ctx = (VGMContext*)context;
+  virtual int ReadPCM(uint8_t* buffer, int size, int& actualsize) override
+  {
+    render_vgmstream((sample*)buffer, size/(2*ctx.stream->channels), ctx.stream);
+    actualsize = size;
 
-  close_vgmstream(ctx->stream);
-  delete ctx;
+    ctx.pos += size;
+    return 0;
+  }
 
-  return true;
-}
+  virtual int64_t Seek(int64_t time) override
+  {
+    int16_t* buffer = new int16_t[576*ctx.stream->channels];
+    if (!buffer)
+      return 0;
 
-bool ReadTag(const char* strFile, char* title, char* artist,
-             int* length)
+    long samples_to_do = (long)time * ctx.stream->sample_rate / 1000L;
+    if (samples_to_do < ctx.stream->current_sample )
+      reset_vgmstream(ctx.stream);
+    else
+      samples_to_do -= ctx.stream->current_sample;
+
+    while (samples_to_do > 0)
+    {
+      long l = samples_to_do>576?576:samples_to_do;
+      render_vgmstream(buffer, l, ctx.stream);
+      samples_to_do -= l;
+    }
+    delete[] buffer;
+
+    return time;
+  }
+
+private:
+  VGMContext ctx;
+};
+
+
+class ATTRIBUTE_HIDDEN CMyAddon : public kodi::addon::CAddonBase
 {
-  return true;
-}
+public:
+  CMyAddon() { }
+  virtual ADDON_STATUS CreateInstance(int instanceType, std::string instanceID, KODI_HANDLE instance, KODI_HANDLE& addonInstance) override
+  {
+    addonInstance = new CVGMCodec(instance);
+    return ADDON_STATUS_OK;
+  }
+  virtual ~CMyAddon()
+  {
+  }
+};
 
-int TrackCount(const char* strFile)
-{
-  return 1;
-}
-}
+
+ADDONCREATOR(CMyAddon);
