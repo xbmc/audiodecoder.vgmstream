4958d57359e6a88f81c3a9521e0d9918b3342bb6
Alwin Esch <alwin.esch@web.de>
2019-08-27 05:09:00 +0200

    remove not needed 'virtual'

--- src/VGMCodec.cpp
+++ src/VGMCodec.cpp
@@ -104,7 +104,7 @@ public:
   CVGMCodec(KODI_HANDLE instance) :
     CInstanceAudioDecoder(instance) {}
 
-  virtual ~CVGMCodec()
+  ~CVGMCodec() override
   {
     if (ctx.stream)
       close_vgmstream(ctx.stream);
@@ -112,11 +112,11 @@ public:
     delete ctx.file;
   }
 
-  virtual bool Init(const std::string& filename, unsigned int filecache,
-                    int& channels, int& samplerate,
-                    int& bitspersample, int64_t& totaltime,
-                    int& bitrate, AEDataFormat& format,
-                    std::vector<AEChannel>& channellist) override
+  bool Init(const std::string& filename, unsigned int filecache,
+            int& channels, int& samplerate,
+            int& bitspersample, int64_t& totaltime,
+            int& bitrate, AEDataFormat& format,
+            std::vector<AEChannel>& channellist) override
   {
     open_VFS((struct _STREAMFILE*)&ctx, filename.c_str(), 0);
 
@@ -152,7 +152,7 @@ public:
     return true;
   }
 
-  virtual int ReadPCM(uint8_t* buffer, int size, int& actualsize) override
+  int ReadPCM(uint8_t* buffer, int size, int& actualsize) override
   {
     render_vgmstream((sample*)buffer, size/(2*ctx.stream->channels), ctx.stream);
     actualsize = size;
@@ -161,7 +161,7 @@ public:
     return 0;
   }
 
-  virtual int64_t Seek(int64_t time) override
+  int64_t Seek(int64_t time) override
   {
     int16_t* buffer = new int16_t[576*ctx.stream->channels];
     if (!buffer)
@@ -193,14 +193,12 @@ class ATTRIBUTE_HIDDEN CMyAddon : public kodi::addon::CAddonBase
 {
 public:
   CMyAddon() { }
-  virtual ADDON_STATUS CreateInstance(int instanceType, std::string instanceID, KODI_HANDLE instance, KODI_HANDLE& addonInstance) override
+  ADDON_STATUS CreateInstance(int instanceType, std::string instanceID, KODI_HANDLE instance, KODI_HANDLE& addonInstance) override
   {
     addonInstance = new CVGMCodec(instance);
     return ADDON_STATUS_OK;
   }
-  virtual ~CMyAddon()
-  {
-  }
+  ~CMyAddon() override = default;
 };
 
 
